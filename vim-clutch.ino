661427eae83ddf81f5120a3a8238e79d7121fba6
Jam Risser <jamrizzi@gmail.com>
2016-11-27 15:24:53 -0600

    Initial working version

---
+++ vim-clutch.ino
@@ -0,0 +1,43 @@
+#include <Bounce.h>
+
+int led = 11;
+int footPedal = 9;
+char mode = 'n';
+
+Bounce button = Bounce(footPedal, 10);
+
+void setup() {
+    Serial.begin(9600);
+    pinMode(footPedal, INPUT_PULLUP);
+    pinMode(led, OUTPUT);
+    delay(1000);
+}
+
+void loop() {
+    button.update();
+    if (button.fallingEdge()) {
+        if (mode == 'i') {
+            normalMode();
+        } else {
+            insertMode();
+        }
+    }
+}
+
+void insertMode() {
+    Keyboard.press(KEY_LEFT_CTRL);
+    Keyboard.press('[');
+    Keyboard.releaseAll();
+    delay(100);
+    Keyboard.write("i");
+    digitalWrite(led, HIGH);
+    mode = 'i';
+}
+
+void normalMode() {
+    Keyboard.press(KEY_LEFT_CTRL);
+    Keyboard.press('[');
+    Keyboard.releaseAll();
+    digitalWrite(led, LOW);
+    mode = 'n';
+}
